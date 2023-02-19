609441c075da630a3c3363e8debf9b1eba721cdd
Pablo Delgado Krämer <private@pablode.com>
2023-02-19 16:46:03 +0100

    Add bitangent input to normalmap node (WIP)

--- libraries/stdlib/genglsl/mx_normalmap.glsl
+++ libraries/stdlib/genglsl/mx_normalmap.glsl
@@ -1,16 +1,12 @@
-void mx_normalmap(vec3 value, int map_space, float normal_scale, vec3 N, vec3 T,  out vec3 result)
+void mx_normalmap(vec3 value, int map_space, float normal_scale, vec3 N, vec3 T, vec3 B, out vec3 result)
 {
-    // Decode the normal map.
-    value = (value == vec3(0.0f)) ? vec3(0.0, 0.0, 1.0) : value * 2.0 - 1.0;
+    // TODO: do we want to add this 0-check to the other backends, too?
+    vec3 n = (value == vec3(0.0f)) ? vec3(0.0, 0.0, 1.0) : value * 2.0 - 1.0;
 
-    // Transform from tangent space if needed.
     if (map_space == 0)
     {
-        vec3 B = normalize(cross(N, T));
-        value.xy *= normal_scale;
-        value = T * value.x + B * value.y + N * value.z;
+        n = T * n.x * normal_scale + B * n.y * normal_scale + N * n.z;
     }
 
-    // Normalize the result.
-    result = normalize(value);
+    result = normalize(n);
 }
